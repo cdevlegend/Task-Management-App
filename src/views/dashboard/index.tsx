803363ad60cf30e3f2c3f9afb7c0cd4f10f1e390
devlegend <arnold.feng@gridsolutions.co.jp>
2023-09-13 09:34:45 +0900

    task app init commit

---
+++ src/views/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Grid } from "@mui/material";
+import Page from "src/components/layouts/Page";
+
+const Dashboard = () => {
+  return (
+    <Page title="Dashboard">
+      <Grid>task</Grid>
+    </Page>
+  );
+};
+
+export default Dashboard;
